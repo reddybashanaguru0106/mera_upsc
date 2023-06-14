88f0711dc82ec8b89aca8a3ead32c755843b75dc
reddy basha naguru <nagurunabirasool0106@gmail.com>
2023-06-14 18:21:23 +0530

    Firebase - Authentication and cloud firestorage integration

---
+++ lib/profile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mera_upsc/navbar.dart';
+
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AppBarExample(),
+    );
+  }
+}
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Center(child: Text('MERA UPSC')),
+          toolbarHeight: 50,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('notification')));
+              },
+            ),
+          ]),
+    );
+  }
+}
