a2daab14e9f3d9c335e7244b787e6109f1ef74b9
Manikanta Reddy Rami Reddy <ashokreddyramireddy0106@gmail.com>
2023-06-19 15:41:24 +0530

    feat/login-firebase

--- lib/homePage/content.dart
+++ lib/homePage/content.dart
@@ -7,34 +7,78 @@ class MyDashboardBody extends StatelessWidget{
    Widget build(BuildContext context){
      return Container(
       margin: const EdgeInsets.symmetric(vertical: 20.0),
-      height: 200,
+      height: 150,
        child: ListView(
               // This next line does the trick.
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                SizedBox(
+                Container(
                   width: 400,
-                  child: Image.asset('././images/sampleProfile.png', scale: 100,),
+                  // color: Colors.blueGrey,
+                  // child: Image.asset('././images/sampleProfile.png', scale: 400,),
+                  padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color:const Color.fromARGB(255, 247, 227, 227)
+                  ,
+                  border: Border.all(
+                    color: Colors.white70,
+      width: 5,
+    ),
+  
+    //     ,BoxShadow ,BoxShadow(
+    //      {Color color: const Color(0xFF000000),
+    //       Offset offset: Offset.zero,
+    //       double blurRadius: 0.0,
+    //       double spreadRadius: 0.0}
+    //  )
+                   ),
+                  
                 ),
-                SizedBox(
+                Container(
                   width: 400,
-                  child: Image.asset('././images/sampleProfile.png'),
-                  // color: Colors.blue,
+                  // child: Image.asset('././images/sampleProfile.png'),
+                  // color: Colors.black87,
+                   padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color:const Color.fromARGB(255, 247, 227, 227)
+                  ,
+                   border: Border.all(
+                    color: Colors.white70,
+      width: 5,
+    ),),
+                  
                 ),
-                SizedBox(
+                Container(
                   width: 400,
-                  // color: Colors.green,
-                  child: Image.asset('././images/sampleProfile.png'),
+                  // color: Colors.white,
+                  // child: Image.asset('././images/sampleProfile.png'),
+                   padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color:const Color.fromARGB(255, 247, 227, 227)
+                  ,
+                    border: Border.all(
+                    color: Colors.white70,
+      width: 5,
+    ),),
                 ),
-                SizedBox(
+                Container(
                   width: 400,
-                  // color: Colors.yellow,
-                  child: Image.asset('././images/sampleProfile.png'),
+                  // color: Colors.amberAccent,
+                  // child: Image.asset('././images/sampleProfile.png'),
+                   padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color:const Color.fromARGB(255, 247, 227, 227),
+                    border: Border.all(
+                    color: Colors.white70,
+      width: 5,
+    ),),
                 ),
-                SizedBox(
+                Container(
                   width: 400,
-                  // color: Colors.orange,
-                  child: Image.asset('././images/sampleProfile.png'),
+                  // color: Colors.redAccent,
+                  // child: Image.asset('././images/sampleProfile.png'),
+                   padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color:const Color.fromARGB(255, 247, 227, 227),
+                    border: Border.all(
+                    color: Colors.white70,
+      width: 5,
+    ),),
                 ),
               ],
             ),
