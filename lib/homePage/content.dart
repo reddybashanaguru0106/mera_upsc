f71b2e2aa18761f4c7f4f8886ce8a91910eb146d
hariraju0106 <135624000+hariraju0106@users.noreply.github.com>
2023-06-20 14:26:01 +0530

    fix | moved navbar and navigation to maindart file

--- lib/homePage/content.dart
+++ lib/homePage/content.dart
@@ -1,46 +1,115 @@
 import 'package:flutter/material.dart';
 
-class MyDashboardBody extends StatelessWidget{
-   const MyDashboardBody({super.key});
+class MyDashboardBody extends StatelessWidget {
+  const MyDashboardBody({super.key});
 
-   @override
-   Widget build(BuildContext context){
-     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 20.0),
-      height: 200,
-       child: ListView(
-              // This next line does the trick.
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                SizedBox(
-                  width: 400,
-                  child: Image.asset('././images/sampleProfile.png', scale: 100,),
-                ),
-                SizedBox(
-                  width: 400,
-                  child: Image.asset('././images/sampleProfile.png'),
-                  // color: Colors.blue,
-                ),
-                SizedBox(
-                  width: 400,
-                  // color: Colors.green,
-                  child: Image.asset('././images/sampleProfile.png'),
-                ),
-                SizedBox(
-                  width: 400,
-                  // color: Colors.yellow,
-                  child: Image.asset('././images/sampleProfile.png'),
-                ),
-                SizedBox(
-                  width: 400,
-                  // color: Colors.orange,
-                  child: Image.asset('././images/sampleProfile.png'),
-                ),
-              ],
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 50.0),
+      height: 150,
+      child: ListView(
+        // This next line does the trick.
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 400,
+            // child: Image.asset(
+            //   '././images/sampleProfile.png',
+            //   scale: 100,
+            // ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.lightBlueAccent,
+              border: Border.all(
+                color: const Color.fromARGB(179, 7, 0, 4),
+                width: 5,
+              ),
             ),
-     );
-    }
+
+            child: const Center(
+              child: Text(
+                  "The UPSC exams are tough tests to get important government jobs in India. There are three stages: Prelims, Mains, and Interview. ",
+                  style: TextStyle(fontSize: 15)),
+            ),
+          ),
+          Container(
+            width: 400,
+            // child: Image.asset('././images/sampleProfile.png'),
+            // color: Colors.blue, //
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.lightGreenAccent,
+              border: Border.all(
+                color: const Color.fromARGB(179, 5, 0, 0),
+                width: 5,
+              ),
+            ),
+            child: const Center(
+                child: Text(
+              'The exams cover many subjects like history, geography, and current affairs.',
+              style: TextStyle(fontSize: 15),
+            )),
+          ),
+          Container(
+            width: 400,
+            // color: Colors.green,
+            // child: Image.asset('././images/sampleProfile.png'),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.lightBlueAccent,
+              border: Border.all(
+                color: const Color.fromARGB(179, 0, 0, 0),
+                width: 5,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                'People work hard to prepare for them. Clearing the exams opens up great career opportunities in the government.',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+          Container(
+            width: 400,
+            // color: Colors.yellow,
+            // child: Image.asset('././images/sampleProfile.png'),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.lightGreenAccent,
+              border: Border.all(
+                color: const Color.fromARGB(179, 0, 0, 0),
+                width: 5,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                ' People spend a lot of time preparing for them. If you pass, you can have a good career in the government.',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+          Container(
+            width: 400,
+            // color: Colors.orange,
+            // child: Image.asset('././images/sampleProfile.png'),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.lightBlueAccent,
+              border: Border.all(
+                color: const Color.fromARGB(179, 4, 0, 0),
+                width: 5,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                'UPSC exams are challenging tests for government jobs in India. They have three stages: Prelims, Mains, and Interview. ',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
-
-
--- lib/navbar.dart
+++ lib/navbar.dart
@@ -20,76 +20,71 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     // final color = Theme.of(context).colorScheme.primary;
 
-
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children:<Widget> [
-          Container(
-            width:double.infinity,
-            padding:const EdgeInsets.all(20),
-            // color: Theme.of(context).primaryColor,
-            decoration:const  BoxDecoration(color: Colors.pink),
-            child:  Center(
-              child:Column(
-                children: <Widget>[
-                  Container(
-                        // color: Colors.black87,
-                         width: 100,
-                         height: 100,
-                         margin:const  EdgeInsets.only(top: 30,
-                         bottom: 10,
-                         ),
-                         decoration:const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(image: AssetImage('./images/sampleProfile.png'),
-                          fit: BoxFit.fill
-                          ), 
-                             ),
-                         
-                  ),
-                   const Text('S.Ganesh',style:
-                         TextStyle( fontSize: 20,
-                                    color: Colors.white70,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                   ),
-                    const Text('sampathiganesh0106@gmail.com',style:TextStyle(
-                              color: Colors.white70,
-                              fontWeight: FontWeight.bold
-                   ),),
-                ],
-              ) 
+      child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          // color: Theme.of(context).primaryColor,
+          decoration: const BoxDecoration(color: Colors.pink),
+          child: Center(
+              child: Column(
+            children: <Widget>[
+              Container(
+                // color: Colors.black87,
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 10,
+                ),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('./images/sampleProfile.png'),
+                      fit: BoxFit.fill),
+                ),
               ),
-        
+              const Text(
+                'S.Ganesh',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'sampathiganesh0106@gmail.com',
+                style: TextStyle(
+                    color: Colors.white70, fontWeight: FontWeight.bold),
+              ),
+            ],
+          )),
         ),
-        
-           const ListTile(
+        const ListTile(
           leading: Icon(Icons.person_2_outlined),
           title: Text('Profile'),
           selectedColor: Colors.amber,
           selectedTileColor: Colors.amber,
-          
-          ),
-
-          const ListTile(
+        ),
+        const ListTile(
           leading: Icon(Icons.settings),
           title: Text('Settings'),
-        
-                //       UserAccountsDrawerHeader(
-                //     accountName:  Padding(
-                //       padding: EdgeInsets.all(2.0),
-                //       child: Text('Name : Ganesh'),
-                       
-                //     ), 
-                //     accountEmail: Padding(
-                //       padding: EdgeInsets.all(2.0),
-                //       child: Text('Email : sampathiganesh0106@gmail.com'),
-                //     ),
-                //     decoration: BoxDecoration(color: Colors.pink),
-                //     arrowColor: Colors.black,
-                // ),
-                //     currentAccountPicture:CircleAvatar(
+
+          //       UserAccountsDrawerHeader(
+          //     accountName:  Padding(
+          //       padding: EdgeInsets.all(2.0),
+          //       child: Text('Name : Ganesh'),
+
+          //     ),
+          //     accountEmail: Padding(
+          //       padding: EdgeInsets.all(2.0),
+          //       child: Text('Email : sampathiganesh0106@gmail.com'),
+          //     ),
+          //     decoration: BoxDecoration(color: Colors.pink),
+          //     arrowColor: Colors.black,
+          // ),
+          //     currentAccountPicture:CircleAvatar(
 //                            child: ClipOval(
 //                              child: Center(
 //                               child: Stack(
@@ -123,13 +118,12 @@ class NavBar extends StatelessWidget {
           //       Navigator.pop(context);
           //     },
           //   ),]
-
-    ),
-    const ListTile(
+        ),
+        const ListTile(
           leading: Icon(Icons.logout_outlined),
           title: Text('Logout'),
-    )
-    ]),
+        )
+      ]),
     );
   }
 
@@ -137,13 +131,12 @@ class NavBar extends StatelessWidget {
     final image = NetworkImage(imagePath);
 
     return ClipOval(
-      
       child: Material(
         color: Colors.transparent,
-        child: 
-        // Image.asset('images/sampleProfile.png', fit:BoxFit.fill)
-        // Image(image:AssetImage('images/sampleProfile.png'))
-        Ink.image(
+        child:
+            // Image.asset('images/sampleProfile.png', fit:BoxFit.fill)
+            // Image(image:AssetImage('images/sampleProfile.png'))
+            Ink.image(
           image: image,
           fit: BoxFit.cover,
           width: 128,
@@ -179,116 +172,5 @@ class NavBar extends StatelessWidget {
           color: color,
           child: child,
         ),
-        // ListTile(
-        //   leading: Icon(Icons.logout_outlined),
-        //   title: Text('logout'),
-        //   )
       );
 }
-
-
-// import 'package:flutter/material.dart';
-
-// class NavBar extends StatelessWidget {
-//   const NavBar({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     //  final color = Theme.of(context).colorScheme.primary;
-//     return  Drawer(
-//       child: ListView(
-//         children:  [UserAccountsDrawerHeader(accountName:const Text('name : Ganesh'), accountEmail:const Text('Mail : sampathiganesh0106@gmail.com'),
-//         currentAccountPicture:CircleAvatar(
-//           child: ClipOval(
-//             child: Center(
-//               child: Stack(
-//                 children: [
-//                   const Image(image:AssetImage('images/sampleProfile.png'),
-//                   width: 80,
-//                   height: 80,
-//                   fit: BoxFit.fitWidth,
-//                   ),
-//                   Positioned(
-//                     bottom: 0,
-//                     right: 0,
-//                     child: Container(
-//                       height: 40,
-//                       width: 40,
-//                       decoration: BoxDecoration(
-//                         shape: BoxShape.circle,
-//                         border: Border.all(
-//                           width: 4,
-//                           color: Colors.white
-//                         ),
-//                         color: Colors.blue
-//                       ),
-//                       child:const Icon(
-//                         Icons.edit,
-//                         color: Colors.white70,
-                        
-//                       ),
-//                     ),
-                    
-//                      ),
-//                     ],
-//                    ),
-//             )
-//                ),  
-//              ),
-//         )],
-
-//       ),
-//     );
-      
-//   }
-
-  
-//   // Widget buildEditIcon(Color color) { 
-//   //   return buildCircle(
-//   //       color: Colors.white,
-//   //       all: 2,
-//   //       child: buildCircle(
-//   //         color: Colors.red,
-//   //         all: 8,
-//   //         child: const Icon(
-//   //           Icons.edit,
-//   //           color: Colors.white,
-//   //           size: 15,
-//   //         ),
-//   //       ),
-//   //     );}
-
-//     Widget buildCircle({
-//       required Widget child,
-//       required double all,
-//       required Color color,
-//     }) {
-//         return ClipOval(
-//           child: Container(
-//             padding: EdgeInsets.all(all),
-//             color: color,
-//             child: child,
-//           ),
-//         );
-//   }
-// }
-
-
-
-
-                    // currentAccountPicture: CircleAvatar(
-                    //   child: 
-                    //   ClipOval(
-                    //     child: 
-                    //     Stack(
-                    //         children: [
-                    //           buildImage(),
-                    //           Positioned(
-                    //             bottom: 0,
-                    //             right: 4,
-                    //             child: buildEditIcon(color),
-                    //           ),
-                    //         ],
-                    //      ),
-                    //   ),
-                    // ),
